0820a216e35a27e492f276c0a3dfa3552a33d327
mirco <mircog@net.chalmers.se>
2022-09-24 16:10:19 +0200

    addMarkers() function debugging information

--- Source Code/Park4Free/app/src/main/java/com/example/park4free/MapsActivity.java
+++ Source Code/Park4Free/app/src/main/java/com/example/park4free/MapsActivity.java
@@ -1,10 +1,17 @@
 package com.example.park4free;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,36 +39,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     //get last known location of the user
 
-    public void addMarkers(GoogleMap mMap){
+    public void addMarkers(GoogleMap mMap) {
         String line;
         StringBuffer response = new StringBuffer();
         HttpURLConnection connection;
 
+        //koden kastar exception och därför syns inte parkeringsplatserna
+        //Lösning
+        //1. Förberedd JSON fil
+        //2. Testa köra på fysisk android enhet.
         try {
             URL source = new URL("http://data.goteborg.se/ParkingService/v2.1/PublicTimeParkings/c9848a32-3248-48e7-a28e-575e2d6d2889?latitude=57.7&longitude=11.9&radius=500&format=JSON");
             connection = (HttpURLConnection) source.openConnection();
             //Request setup
             connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
+            connection.setConnectTimeout(10000);
+            connection.setReadTimeout(10000);
 
             int status = connection.getResponseCode();
             System.out.println(status);
 
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while ((line=bufferedReader.readLine()) != null){
+            while ((line = bufferedReader.readLine()) != null) {
                 response.append(line);
             }
             bufferedReader.close();
             System.out.println(response.toString());
             JSONArray jsonArray = new JSONArray(response.toString());
-            for (int i=0; i<jsonArray.length();i++){
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 double lat = jsonObject.getDouble("Lat");
                 double lng = jsonObject.getLong("Long");
-                Log.i("Test","Hello 123456");
-                Log.i("Latitude",lat+"");
-                Log.i("Longitude",lng+"");
+                Log.i("Test", "Hello 123456");
+                Log.i("Latitude", lat + "");
+                Log.i("Longitude", lng + "");
                 LatLng parkeringGbg = new LatLng(lat, lng);
                 MarkerOptions mk = new MarkerOptions().position(parkeringGbg).title("Marker in Sydney");
                 mMap.addMarker(mk);
@@ -91,6 +102,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    //Checking, Asking and Setting Permissions
+    private boolean checking4Permissions () {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+            return true;
+        return false;
+    }
+    private void ask4Permissions () {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                1);
+
+        if (!checking4Permissions()) {
+            Toast.makeText(this, "You cannot use the app without granting the necessary permissions!", Toast.LENGTH_SHORT).show();
+            openAppSettings();
+        }
+    }
+    public void openAppSettings() {
+
+        Uri packageUri = Uri.fromParts( "package", getApplicationContext().getPackageName(), null );
+
+        Intent applicationDetailsSettingsIntent = new Intent();
+
+        applicationDetailsSettingsIntent.setAction( Settings.ACTION_APPLICATION_DETAILS_SETTINGS );
+        applicationDetailsSettingsIntent.setData( packageUri );
+        applicationDetailsSettingsIntent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+
+        getApplicationContext().startActivity( applicationDetailsSettingsIntent );
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -105,6 +148,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+/*        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }*/
+        //ask4Permissions();
+        //mMap.setMyLocationEnabled(true);
+
+        addMarkers(mMap);
 
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -113,8 +170,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng australia = new LatLng(-30, 145);
         mMap.addMarker(new MarkerOptions().position(australia).title("Marker in sweden"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sweden));
-
-        addMarkers(mMap);
 
 
 
