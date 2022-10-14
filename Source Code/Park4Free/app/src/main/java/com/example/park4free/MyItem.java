004f31f10519367d00c624bfcb66a918b151bbca
mirco <mircog@net.chalmers.se>
2022-10-14 20:25:57 +0200

    Added file for clustering marker icons
    //Contributors: Mirco, Abbas, Mustafa, Love

---
+++ Source Code/Park4Free/app/src/main/java/com/example/park4free/MyItem.java
@@ -0,0 +1,62 @@
+package com.example.park4free;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+public class MyItem implements ClusterItem {
+
+    private LatLng position;
+    public String title;
+    private String snippet;
+    public int profilePhoto;
+
+    public MyItem(double lat, double lng){
+        position = new LatLng(lat,lng);
+        title = null;
+        snippet = null;
+    }
+
+    public MyItem(double lat, double lng, String title, String snippet, int profilePhoto){
+        position = new LatLng(lat,lng);
+        this.title = title;
+        this.snippet = snippet;
+        this.profilePhoto = profilePhoto;
+    }
+
+    @NonNull
+    @Override
+    public LatLng getPosition() {
+        return position;
+    }
+
+    @Nullable
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Nullable
+    @Override
+    public String getSnippet() {
+        return snippet;
+    }
+
+    /**
+     * Set the title of the marker
+     * @param title string to be set as title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Set the description of the marker
+     * @param snippet string to be set as snippet
+     */
+    public void setSnippet(String snippet) {
+        this.snippet = snippet;
+    }
+}
