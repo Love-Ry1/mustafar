758b34ec0a728289fc4c14067f6190aa573ceb10
mirco <mircog@net.chalmers.se>
2022-10-17 16:26:42 +0200

    test
    //Contributors: Mirco, Abbas, Mustafa, Love

--- Source Code/Park4Free/app/src/main/java/com/example/park4free/MapsActivity.java
+++ Source Code/Park4Free/app/src/main/java/com/example/park4free/MapsActivity.java
@@ -90,7 +90,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    //test
 
     //Search related
     private RecyclerView recyclerView;
